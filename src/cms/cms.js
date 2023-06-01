2569ae5129ceffad128b1c63dd0767b8b8fc5930
jmcglone <jmcglone@umich.edu>
2023-06-01 09:46:42 -0400

    use GitHub auth for CMS

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -13,14 +13,9 @@ if (process.env.BRANCH === "preview") {
   CMS.init({
     config: {
       backend: {
-        name: 'gitlab',
-        repo: 'michigan-publishing/about-bridwell',
-        branch: 'preview',
-        auth_type: 'pkce',
-        app_id: '80d4d50a58055640e90d01ddb39f58ce6248a8f1153d9068df828230488536b7',
-        api_root: 'https://gitlab.umich.edu/api/v4',
-        base_url: 'https://gitlab.umich.edu',
-        auth_endpoint: 'oauth/authorize'
+        name: 'github',
+        repo: 'mlibrary/about-bridwell',
+        branch: 'preview'
       }
     }
   });
@@ -28,14 +23,9 @@ if (process.env.BRANCH === "preview") {
   CMS.init({
     config: {
       backend: {
-        name: 'gitlab',
-        repo: 'michigan-publishing/about-bridwell',
-        branch: 'main',
-        auth_type: 'pkce',
-        app_id: '80d4d50a58055640e90d01ddb39f58ce6248a8f1153d9068df828230488536b7',
-        api_root: 'https://gitlab.umich.edu/api/v4',
-        base_url: 'https://gitlab.umich.edu',
-        auth_endpoint: 'oauth/authorize'
+        name: 'github',
+        repo: 'mlibrary/about-bridwell',
+        branch: 'main'
       }
     }
   });
